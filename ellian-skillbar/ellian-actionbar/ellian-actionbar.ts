c31542ebb5e1a1000b0368da8d7ec1a29921e9a0
Ellian <ellian81@hotmail.com>
2015-09-09 11:54:18 +0200

    fix drawer position

--- ellian-skillbar/ellian-actionbar/ellian-actionbar.ts
+++ ellian-skillbar/ellian-actionbar/ellian-actionbar.ts
@@ -484,6 +484,7 @@ module EllianActionbar {
     }
 
     function triggerDrawer() {
+        if (tooltipOpenDrawer) tooltipOpenDrawer.destroy();
         if (isDrawerDisplayed) {
             isDrawerDisplayed = false;
             displayClosedDrawer();
@@ -494,15 +495,20 @@ module EllianActionbar {
     }
 
     function displayOpenedDrawer() {
+        if (tooltipOpenDrawer) tooltipOpenDrawer.destroy();
         $drawer.empty();
         var w:number = 4;
-        $drawer.css('width', w * 30);
-        $drawer.css('height', (Math.round(abilities.length / w) + 1) * 30);
+        $drawer.css('width', parseInt($actionsbars.css('width').replace('px', '')));
+        $drawer.css('height', (Math.round(abilities.length / w)) * 30 + 2);
         $drawer.css('left', -w * 30);
-        $drawer.css('top', -(Math.round(abilities.length / w) + 1) * 30);
-        $drawer.css('background-image', "url('../images/spellbook/left-page.png')");
-        $drawer.css('padding-right', '2px');
+        $drawer.css('top', -(Math.round(abilities.length / w)) * 30 + 2);
 
+        var slots = document.createElement('div');
+        $(slots).css('width', w * 30);
+        $(slots).css('height', (Math.round(abilities.length / w)) * 30 + 2);
+        $(slots).css('background-image', "url('../images/spellbook/left-page.png')");
+        $(slots).css('padding-right', '2px');
+        $drawer.append(slots);
         abilities.forEach(function (ability, i) {
             var abil = document.createElement('div');
             $(abil).css('content', "url('" + ability.icon + "')");
@@ -514,7 +520,7 @@ module EllianActionbar {
             $(abil).on('mousedown', mouseDown);
             $(abil).attr(ATTR_NUM, ability.id);
             $(abil).attr(ATTR_TYPE, TYPE_ABILITY);
-            $drawer.append(abil);
+            $(slots).append(abil);
         });
         if (tooltipDrawer) tooltipDrawer.destroy();
         tooltipDrawer = new Tooltip($drawer.children(), {leftOffset: 0, topOffset: -30});
@@ -525,7 +531,7 @@ module EllianActionbar {
         $(close).attr('src', '../images/spellbook/btn-close.png');
         $(close).attr('height', '12px').attr('width', '12px');
         $(close).on('click', triggerDrawer);
-        var tooltipOpenDrawer = new Tooltip($(close), {
+        tooltipOpenDrawer = new Tooltip($(close), {
             title: "Close",
             content: "Close the drawer.",
             leftOffset: 0,
@@ -534,14 +540,21 @@ module EllianActionbar {
         $drawer.append(close);
         $(close).off('click');
         $(close).on('click', triggerDrawer);
+
+        /*      updateActionbarHeight(barConfig.isBar2Displayed);
+         console.log(parseInt($actionbar.css('height').replace('px', '')));
+         $actionbar.css('height', (Math.round(abilities.length / w)) * 30 + parseInt($actionbar.css('height').replace('px', '')));
+         $actionsbars.css('height', (Math.round(abilities.length / w)) * 30 + parseInt($actionsbars.css('height').replace('px', '')));
+         */
     }
 
     function displayClosedDrawer() {
+        if (tooltipOpenDrawer) tooltipOpenDrawer.destroy();
         $drawer.empty();
         var open = document.createElement('img');
         $(open).attr('src', '../images/spellbook/btn-add-bookmark.jpg');
         $(open).attr('height', '18px').attr('width', '18px');
-        var tooltipOpenDrawer = new Tooltip($(open), {
+        tooltipOpenDrawer = new Tooltip($(open), {
             title: "Open",
             content: "Open the drawer containing the available abilities and commands.",
             leftOffset: 0,
@@ -549,13 +562,15 @@ module EllianActionbar {
         });
         $drawer.css('left', '-20px');
         $drawer.css('top', '26px');
-        $drawer.css('background-image', "url('../images/spellbook/left-page.png')");
+        //$drawer.css('background-image', "url('../images/spellbook/left-page.png')");
         $drawer.css('width', '18px');
-        $drawer.css('height', '18px');
+        $drawer.css('height', '0px');
         $drawer.css('padding-right', '0px');
         $drawer.append(open);
         $(open).off('click');
         $(open).on('click', triggerDrawer);
+
+        updateActionbarHeight(barConfig.isBar2Displayed);
     }
 
     function init() {
@@ -624,15 +639,15 @@ module EllianActionbar {
     /* Initialization */
     if (cu.HasAPI()) {
         cu.OnInitialized(() => {
-                cu.RequestAbility(BANDAGE_ABILITY_ID, ability => {
-                    ability.icon = '../images/skills/bandage.png';
-                    abilities.unshift(ability);
-                }, true);
                 cuAPI.CloseUI("skillbar");
                 cuAPI.CloseUI("bandage");
                 cuAPI.OnCharacterIDChanged(onCharacterIDChanged);
                 cuAPI.OnAbilityCreated(onAbilityCreated);
                 cuAPI.OnAbilityDeleted(onAbilityDeleted);
+                cu.RequestAbility(BANDAGE_ABILITY_ID, ability => {
+                    ability.icon = '../images/skills/bandage.png';
+                    abilities.unshift(ability);
+                }, true);
                 init();
             }
         );
